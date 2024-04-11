bcc02db7f1de980262f62b03bad2c56bb7d3e207
Jeffrey <9014223@student.zadkine.nl>
2024-04-11 15:00:56 +0200

    Update shards.go

--- shards.go
+++ shards.go
@@ -10,7 +10,7 @@ import (
 
 const (
 	// TIMELIMIT specifies how long to pause between connecting shards.
-	TIMELIMIT = time.Millisecond * 500
+	TIMELIMIT = time.Second * 1
 	// VERSION specifies the shards module version. Follows semantic versioning (semver.org).
 	VERSION = "2.4.0"
 )
